80db32187bc1bbbf81944f913d63105c20ae81f2
Camren Hall <camrenhall@gmail.com>
2025-08-16 19:53:06 -0500

    Remove OpenAIDocumentAnalysisTool import (unused)

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -4,14 +4,12 @@ Tools package for document analysis.
 
 from .factory import tool_factory
 from .planning import PlanAnalysisTasksTool
-from .analysis import OpenAIDocumentAnalysisTool
 from .storage import StoreAnalysisResultsTool
 from .context import GetCaseContextTool
 
 __all__ = [
     "tool_factory",
     "PlanAnalysisTasksTool",
-    "OpenAIDocumentAnalysisTool", 
     "StoreAnalysisResultsTool",
     "GetCaseContextTool"
 ]
